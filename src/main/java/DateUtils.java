cce9cd8f91e662283a9494e686c469440c96daea
zhaoliang <zhaoliang@coolchuan.net>
2015-05-22 21:23:30 +0800

    fffff

---
+++ src/main/java/DateUtils.java
@@ -0,0 +1,14 @@
+package com.zhaoliang.date;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class DateUtils {
+	public static void main(String[] args) {
+		DateFormat format=new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss  zZ(zzzzz)",new Locale("China"));
+		//format.setTimeZone(TimeZone.getTimeZone("CTS"));
+		System.out.println(format.format(new Date()).toString());
+	}
+}
